3907baca6a5725c967c4cc403618bd682fcbebae
cloudwipe <roma.isaev.2000@gmail.com>
2023-08-17 15:43:25 +0300

    task-02 complete

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsContainer = document.querySelector("#ingredients");
+
+const ingredientsList = [];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const addIngredientsToList = document.createElement("li");
+  addIngredientsToList.textContent = ingredients[i];
+  addIngredientsToList.classList.add("item");
+  console.log(addIngredientsToList);
+
+  ingredientsList.push(addIngredientsToList);
+}
+
+ingredientsContainer.append(...ingredientsList);
