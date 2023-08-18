dc8d2e3b10bed614d4f0330039058338019ef66f
cloudwipe <roma.isaev.2000@gmail.com>
2023-08-18 16:13:30 +0300

    task-04 complete

---
+++ js/task-04.js
@@ -0,0 +1,35 @@
+const counter = {
+  value: 0,
+  increment() {
+    this.value += 1;
+  },
+  decrement() {
+    this.value -= 1;
+  },
+};
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const counterValue = document.querySelector("#value");
+
+decrementBtn.addEventListener("click", () => {
+  counter.decrement();
+  counterValue.textContent = counter.value;
+});
+
+incrementBtn.addEventListener("click", () => {
+  counter.increment();
+  counterValue.textContent = counter.value;
+});
+
+// Я сначала пробовал сделать так
+
+// decrementBtn.addEventListener("click", () => {
+//   counterValue.textContent += 1;
+// });
+
+// incrementBtn.addEventListener("click", () => {
+//   counterValue.textContent -= 1;
+// });
+
+// Но у меня работало только отнимание, а прибавление считало вот так 01111
