404c7fbc49930e6e49c39ead7e620bc55c82467e
cloudwipe <roma.isaev.2000@gmail.com>
2023-08-26 16:52:05 +0300

    task-10 complete

--- js/task-10.js
+++ js/task-10.js
@@ -4,87 +4,40 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-// const takeCountCreateBoxes = document.querySelector("input");
-// takeCountCreateBoxes.addEventListener("input", takeCountBox);
-
-// function takeCountBox(takeCountCreateBoxes) {
-//   return event.currentTarget.value;
-// }
-
-// function createBoxes(amount) {
-//   for (let i = 0; i < amount; i += 1) {
-//     const box = document.createElement("div");
-//     box.setAttribute("width", "30px");
-//     box.setAttribute("height", "30px");
-//   }
-// }
-
-// const boxContainer = document.querySelector("#boxes");
-
 const inputAmountControl = document.querySelector("input");
 console.log(inputAmountControl);
-inputAmountControl.addEventListener("input", getAmount);
 
 const addBoxesButton = document.querySelector("[data-create]");
-addBoxesButton.addEventListener("click", createBoxes);
+addBoxesButton.addEventListener("click", getAmount);
 
 const destroyBoxesButton = document.querySelector("[data-destroy]");
 destroyBoxesButton.addEventListener("click", destroyBoxes);
 
 const boxContainer = document.querySelector("#boxes");
 
+function getAmount() {
+  const amount = inputAmountControl.value;
+  createBoxes(amount);
+}
+
 function createBoxes(amount) {
+  let sizeBox = 30;
+
+  const boxesArray = [];
+
   for (let i = 0; i < amount; i += 1) {
     const box = document.createElement("div");
-    box.style.width = "30px";
-    box.style.height = "30px";
+    box.style.width = `${sizeBox}px`;
+    box.style.height = `${sizeBox}px`;
     box.style.backgroundColor = getRandomHexColor();
 
-    boxContainer.appendChild(box);
+    sizeBox += 10;
+
+    boxesArray.push(box);
   }
+  boxContainer.append(...boxesArray);
 }
 
-function getAmount(element) {
-  const amount = element.currentTarget.value;
-  console.log(amount);
-  return amount;
+function destroyBoxes() {
+  boxContainer.innerHTML = [];
 }
-
-getAmount(inputAmountControl);
-
-function destroyBoxes() {}
-
-// function getAmount(event) {
-//   const amount = event.currentTarget.value;
-//   console.log(amount);
-//   return amount;
-// }
-
-// *** kak to rabotaet
-// const inputAmountControl = document.querySelector("input");
-// console.log(inputAmountControl);
-// inputAmountControl.addEventListener("input", createBoxes);
-
-// const addBoxesButton = document.querySelector("[data-create]");
-// addBoxesButton.addEventListener("click", createBoxes);
-
-// const destroyBoxesButton = document.querySelector("[data-destroy]");
-// destroyBoxesButton.addEventListener("click", destroyBoxes);
-
-// const boxContainer = document.querySelector("#boxes");
-
-// function createBoxes(amount) {
-//   const amountBoxes = amount.currentTarget.value;
-//   console.log(amountBoxes);
-
-//   for (let i = 0; i < amountBoxes; i += 1) {
-//     const box = document.createElement("div");
-//     box.style.width = "30px";
-//     box.style.height = "30px";
-//     box.style.backgroundColor = getRandomHexColor();
-
-//     boxContainer.appendChild(box);
-//   }
-// }
-
-// function destroyBoxes() {}
