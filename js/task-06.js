51bdfaea12898eca0e4904b991d8d2e2cc247087
cloudwipe <roma.isaev.2000@gmail.com>
2023-08-26 20:15:08 +0300

    task-06 update

--- js/task-06.js
+++ js/task-06.js
@@ -4,11 +4,13 @@ const countNumber = document.querySelector('input[data-length="6"]');
 input.addEventListener("blur", checkInputLength);
 
 function checkInputLength() {
-  if (input.value.length < countNumber.dataset.length) {
-    input.classList.add("invalid");
-    input.classList.remove("valid");
-  } else {
+  if (countNumber.dataset.length == input.value.length) {
     input.classList.remove("invalid");
     input.classList.add("valid");
+  } else {
+    input.classList.remove("valid");
+    input.classList.add("invalid");
   }
 }
+
+// думаю можно сделать лучше и короче, но пока не готов :)
