55bbb74908feab3f1ce6e2e33b8a27dca22a4b3d
cloudwipe <roma.isaev.2000@gmail.com>
2023-08-26 20:22:55 +0300

    task-08 update

--- js/task-08.js
+++ js/task-08.js
@@ -1,29 +1,24 @@
 const form = document.querySelector(".login-form");
-console.log(form);
-
 form.addEventListener("submit", onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
 
   const user = {};
-
   const userMail = event.currentTarget.elements.email.value;
-  console.log(userMail);
-
   const userPassword = event.currentTarget.elements.password.value;
-  console.log(userPassword);
 
   if (userMail === "" || userPassword === "") {
     alert("Все поля должны быть заполнены.");
+    return;
   }
 
   user.mail = userMail;
   user.password = userPassword;
 
-  console.log(user);
-
   if (userMail === userMail || userPassword === userPassword) {
     form.reset();
   }
+
+  console.log(user);
 }
